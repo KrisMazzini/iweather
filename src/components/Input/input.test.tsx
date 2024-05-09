8bdb4067cbdd67c36beb1a077463c94d6e592655
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-05-09 09:46:25 -0300

    test(input): implement tests for the input component

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react-native';
+import { Input } from '@components/Input';
+
+describe("Component: Input", () => {
+    it("should render without activityIndicator if isLoading prop is undefined", () => {
+        render(<Input />);
+        
+        const activityIndicator = screen.queryByTestId('activity-indicator');
+        expect(activityIndicator).toBeNull();
+    })
+
+    it("should render without activityIndicator if isLoading prop is false", () => {
+        render(<Input isLoading={false} />);
+
+        const activityIndicator = screen.queryByTestId('activity-indicator');
+        expect(activityIndicator).toBeNull();
+    })
+    
+    it("should render with activityIndicator if isLoading prop is true", () => {
+        render(<Input isLoading />);
+
+        const activityIndicator = screen.getByTestId('activity-indicator');
+        expect(activityIndicator).toBeTruthy();
+    })
+})
