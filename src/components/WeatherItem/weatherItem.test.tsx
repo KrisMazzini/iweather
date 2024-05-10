3b11f3ab8505cbe3322409e328051bea00c34ee4
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-05-10 13:32:57 -0300

    test(weather-item): implement tests for the WeatherItem component

---
+++ src/components/WeatherItem/weatherItem.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react-native"
+import { WeatherItem } from "@components/WeatherItem"
+
+import drop from "@assets/drop.svg"
+
+describe("Component: Weather Item", () => {
+    it("should show title and value", () => {
+        render(
+            <WeatherItem 
+                icon={drop}
+                title="Umidade do ar"
+                value="81%"
+            />
+        )
+
+        const title = screen.getByText("Umidade do ar")
+        const value = screen.getByText("81%")
+
+        expect(title).toBeTruthy()
+        expect(value).toBeTruthy()
+    })
+})
